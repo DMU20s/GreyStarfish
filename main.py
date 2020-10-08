43e6039182553c7ec9fc8b3893f5c86f313a5538
0xcyber-sketch <71249469+0xcyber-sketch@users.noreply.github.com>
2020-10-08 19:17:10 +0200

    update

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 
-from os import path
 
 import GetMousePosition
+import  getRegions
+from pfmqtt import Tx
+
 from Data import add_data, empty_data
 
 
 
+
 if __name__ == "__main__":
-    recorded_list = []
     my_class = GetMousePosition.MouseMonitor()
     my_class.run_listener()
-    screen1, screen2, screen3, screen4, screen5, screen6 = my_class.get_data()
 
-    add_data(screen1, screen2, screen3, screen4, screen5, screen6)
+    t = Tx("pfgs", "mqtt.eclipse.org", "lol")
+    t.send_unenc(str(my_class.get_data()))
+
+
+    print(my_class.get_data())
+    #getRegions.get_region(my_class.get_data())
+
+
+    #add_data(screen1, screen2, screen3, screen4, screen5, screen6)
     #empty_data()
-
 
 
 
