e015d954018116deb204377dd2cde59f94f568f9
0xcyber-sketch <71249469+0xcyber-sketch@users.noreply.github.com>
2020-10-22 09:56:47 +0200

    Update GetMousePosition.py

--- GetMousePosition.py
+++ GetMousePosition.py
@@ -10,6 +10,7 @@ from pfmqtt import Tx
 
 import json
 
+data_list = []
 
 class MouseMonitor:
     """
@@ -21,7 +22,7 @@ class MouseMonitor:
     def __init__(self):
         # Declare variables
         self.run = True
-        self.data_list = []
+        #self.data_list = []
         self.t = Tx("d6f73930-cd18-4021-b28a-1d9a3e733333/6bfc17fb-3c55-4a29-81e7-c55089134ace", "mqtt.eclipse.org",
                     "lol")
 
@@ -31,31 +32,36 @@ class MouseMonitor:
         if pressed:
             # Captures data and converts it to json
             data = json.dumps({"x": x, "y": y, "timestamp": datetime.timestamp(datetime.now())})
-            self.data_list.append(data)
+            data_list.append(data)
+            print(x, y)
 
     def send_thread(self):
-       # if len(self.data_list) > 0:
+        threading.Timer(1, self.send_thread).start()
+        if len(data_list) > 0:
 
-        #self.t.send_unenc(str(self.data_list))
-        print(self.data_list)
-        self.data_list.clear()
-        self.send_thread()
+        #self.t.send_unenc(str(data_list))
+            print(data_list)
+            data_list.clear()
+
+
+
 
     def run_listener(self):
         # Start a listener thread
-        sender = threading.Timer(2, self.send_thread)
-        sender.start()
+
+        self.send_thread()
         listener = Listener(
             on_click=self.on_click
         )
         listener.start()
-        #sender.cancel()
         # This needs to be on a specific time or something
         while self.run:
             sleep(10)  # Amount of time that the listener records the input.
             self.run = False
             # Stop listener
         listener.stop()
+        threading.Thread.
+
 
 # import threading
 # def printit():
